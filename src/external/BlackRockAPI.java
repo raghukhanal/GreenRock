69d493b7546a211e02f57b6f6846278b3b28d6e9
zoecq001 <zoecq001@gmail.com>
2019-10-26 01:33:31 -0400

    update

--- src/external/BlackRockAPI.java
+++ src/external/BlackRockAPI.java
@@ -3,26 +3,41 @@ import java.net.*;
 import java.io.*;
 
 public class BlackRockAPI {
-	public void readData() throws Exception {
-        String urlString = "https://www.blackrock.com/tools/hackathon/search-securities?identifiers=IXN,GS";
+	public void readData(String positions) throws Exception {
+        String urlString = "https://www.blackrock.com/tools/hackathon/portfolio-analysis";
+        urlString += "?positions=" + positions;
+        
+        urlString += "?startDate=" + "20151212";
+        urlString += "?endDate=" + "20151220";
+        urlString += "?currency=" + "USD";
+        urlString += "?calculateRisk=" + "true";
+        urlString += "?calculateExposures=" + "true";
+        urlString += "?calculateExpectedReturns=" + "true";
+        urlString += "?includeReturnsMap=" + "true";
+        urlString += "?returnsType=" + "DAILY";
+        
+        urlString += "?timeout=" + "10000";
+        
         URL url = new URL(urlString);
         URLConnection urlConnection = url.openConnection();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
         String inputLine;
+        //File file = new File("/Users/qinchen/Codes/GreenRock/ex.txt");
+        //PrintWriter p = new PrintWriter(file);
+        
+        
         while ((inputLine = bufferedReader.readLine()) != null) {
+        	//p.write(inputLine);
             System.out.println(inputLine);
         }
         bufferedReader.close();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
     	BlackRockAPI apiReader = new BlackRockAPI();
-        try {
-			apiReader.readData();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        apiReader.readData("BLK~100");
     }
+    
+    
 
 }
